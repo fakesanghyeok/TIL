8b12d1d23430739658184117b83c100299936bfa
fakesanghyeok <s24066@gsm.hs.kr>
2024-05-09 09:24:37 +0900

    &-homework

--- javascript.js
+++
@@ -1,5 +0,0 @@
-const textElement = document.getElementById("name");
-alert ("Lee무위키에 오신것을 환영합니다!!");
-let name = prompt("당신을 뭐라고 부를까요?");
-alert (name + "님 환영합니다!");
-textElement.innerHTML += name;
